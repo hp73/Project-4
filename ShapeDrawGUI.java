7aa7579c1586a34298add3798e7007e97719b0cb
Harry Pinkerton <50304543+hp73@users.noreply.github.com>
2019-11-04 11:55:53 -0500

    Created Buttons for all the colors and shapes

--- ShapeDrawGUI.java
+++ ShapeDrawGUI.java
@@ -39,7 +39,66 @@ public class ShapeDrawGUI extends JFrame {
       positionConst.weighty = 0;
       
       // Add buttons and checkboxes below the canvas here
+      JButton button1 = new JButton("Square");
+      JButton button2 = new JButton("Triangle");
+      JButton button3 = new JButton("Octagon");
+      JButton button4 = new JButton("Circle");
+      JButton button5 = new JButton("Red");
+      JButton button6 = new JButton("Blue");
+      JButton button7 = new JButton("Green");
+      JButton button8 = new JButton("Orange");
+      JButton button9 = new JButton("Pink");
+      JButton button10 = new JButton("Yellow");   
+
+      JPanel panel1 = new JPanel();
+      panel1.add(button1);
+      this.getContentPane().add(panel1);
+      setVisible(true);
       
+      JPanel panel2 = new JPanel();
+      panel2.add(button2);
+      this.getContentPane().add(panel2);
+      setVisible(true);
+      
+      JPanel panel3 = new JPanel();
+      panel2.add(button3);
+      this.getContentPane().add(panel3);
+      setVisible(true);
+      
+      JPanel panel4 = new JPanel();
+      panel2.add(button4);
+      this.getContentPane().add(panel4);
+      setVisible(true);
+      
+      JPanel panel5 = new JPanel();
+      panel2.add(button5);
+      this.getContentPane().add(panel5);
+      setVisible(true);
+      
+      JPanel panel6 = new JPanel();
+      panel2.add(button6);
+      this.getContentPane().add(panel6);
+      setVisible(true);
+      
+      JPanel panel7 = new JPanel();
+      panel2.add(button7);
+      this.getContentPane().add(panel7);
+      setVisible(true);
+      
+      JPanel panel8 = new JPanel();
+      panel2.add(button8);
+      this.getContentPane().add(panel8);
+      setVisible(true);
+      
+      JPanel panel9 = new JPanel();
+      panel2.add(button9);
+      this.getContentPane().add(panel9);
+      setVisible(true);
+      
+      JPanel panel10 = new JPanel();
+      panel2.add(button10);
+      this.getContentPane().add(panel10);
+      setVisible(true);
    }
    
    public static void main(String[] args) {
