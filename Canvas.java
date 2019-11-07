ff41c3f8c7d30da8b95144edf8fb4cf1b004f6f7
Laurie Jones <hannahljones98@gmail.com>
2019-11-07 10:42:01 -0500

    made a checkbox
    got bles this too way too long  not  to see a }

--- Canvas.java
+++ Canvas.java
@@ -6,16 +6,26 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import javax.swing.JPanel;
 import java.awt.event.*;
+import shapes.*;
 
-
+//from the mouse stuff
 public class Canvas extends JPanel implements MouseListener, MouseMotionListener {
    
-   // Variables
+   //Variables
+   //updates the mouse things and "what we want itto be"
    private Point posStart;
    private Point posEnd;   
    private Rectangle drawRect;
+   private String shape;
+   //private Octagon drawPoly;
+   //private Circle drawRound;
+
+
+   
    
    // Final variables
+   //the outline of the rectangle
+   //abstract draw metjod
    final private Color colorSelect = new Color(0, 200, 200);
    
    // Constructors and setup methods
@@ -26,9 +36,14 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
       
       // No selection rectangle to draw
       drawRect = null;
+
+      ////
+      //drawPoly = null;
       
       // Listen for mouse movement or input
-      addMouseListener(this);      
+      //mouse clicks
+      addMouseListener(this);    
+      //mouse movements
       addMouseMotionListener(this);
       
       
@@ -40,24 +55,70 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
       super.paintComponent(g);
       
       // Draw any shapes in the shape holder here
-      
       // Draw drag rectangle if it is there
+
       if (drawRect != null) {
          g.setColor(colorSelect);
-         g.drawRect((int)drawRect.getX(), (int)drawRect.getY(),
-                    (int)drawRect.getWidth(), (int)drawRect.getHeight());
+
+      //a method and instance varible called drawRect, watch out!!!
+      //if method is there you ahve clicked
+      // the method draws the rectangle
+         g.drawRect((int)drawRect.getX(), (int)drawRect.getY(), 
+         (int)drawRect.getWidth(), (int)drawRect.getHeight());
          
          // Draw the current shape here
+
+         //g.drawPoly((int)drawPoly.getX(), (int)drawPoly.getY(), (int)drawPoly.getWidth(), (int)drawPoly.getHeight());
          
       }
       
    }
+
+      //make a draw method wehere you can just 
+   //inherit everything and jut put in which oen you like
+   /*public void draw(Class shape){
+
+      if (shape == Circle ){
+         int x = posEnd.getX();
+         int x2 = posStart.getX();
+         int y = posEnd.getY();
+         int y2 = posStart.getY();
+
+         int width = (int)Math.abs(posEnd.getX() - posStart.getX());
+         int height = (int)Math.abs(posEnd.getX()- posStart.getX());
+         int left = (int)Math.min(posStart.getX(), posEnd.getX());
+         int top = (int)Math.min(posStart.getY(), posEnd.getY());         
+         drawRect.setLocation(left, top);
+         drawRect.setSize(width, height);
+
+      }
+      
+
+   }*/
    
    // Highly recommended to create this method for assisting in the paintComponent method
-   //private void drawGenericShape(Graphics g, GeometricShape s) {
+   /*private void drawGenericShape(Graphics g, GeometricAbstract s) {
+
+
       // Draw any sort of shape to the canvas
+
+      // a bunch of if else statements
+      //geometricAbstract rect- draw ret
+      if (drawRect != null) {
+         g.setColor(colorSelect);
+
+      else if (drawPoly != null) {
+         g.setColor(colorSelect);
+
+      else if (drawRound != null) {
+         g.setColor(colorSelect);
+      //octogon, draw poly
+      //circle, stationary oval
+      // there is a method that will drww a polygon for you in graphics g
+      //if xvoordinates in is array list it needs to be in an array but there should be a conversion
       
-   //}
+      
+   }*/
    
    // Needed for mouse listeners
    @Override
@@ -74,6 +135,9 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
       
    }
    
+   //where we click so where we start drawing out trectable, 
+   //button1 is left
+
    @Override
    public void mousePressed(MouseEvent e) {
       if (e.getButton() == MouseEvent.BUTTON1) {
@@ -82,10 +146,16 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
          
          // Create new shape here
          
-         updateRectangle();
-         
+         updateRectangle(); 
         
       }
+
+      /*if (e.getButton() == MouseEvent.circleButton) {
+         posStart = new Point(e.getX(), e.getY());
+         posEnd = new Point(e.getX(), e.getY());         
+         updateCircle(); 
+        
+      }*/
    }
 
    @Override
@@ -95,13 +165,27 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
          updateRectangle();
          
          // Add the shape to the shapeholder here        
-         
+         // Add the shape to the shapeholder here        
+         // first get the click and drag shape to show, and then goign away before you ge tthem to stay
+         //circel and square always stay circles and squares
+         // the tealoutline will always be there
+         // Free up the draw variables
          // Free up the draw variables
          drawRect = null;
          posStart = null;
          posEnd = null;
          
       }
+
+      /*if (e.getButton() == MouseEvent.circleButton) {
+         posEnd.setLocation(e.getX(), e.getY());
+         updateCircle();
+
+         drawRound = null;
+         posStart = null;
+         posEnd = null;
+         
+      }*/
       
    }
    
@@ -111,6 +195,11 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
          posEnd.setLocation(e.getX(), e.getY());
          updateRectangle();
       }
+      /*if (drawRound != null) {
+         posEnd.setLocation(e.getX(), e.getY());
+         updateCircle();
+      }*/
+
    }
 
 
@@ -122,15 +211,26 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
       
    }
    
+
+   public void setShape(String shape){
+      this.shape = shape;
+   }
+
    // Updates the selection rectangle based on the first and current mouse positions
+
+   //Changing Rectangle
    public void updateRectangle() {
       
       if (drawRect == null) {
+         //make sure that there is a rectangle
+         //x, y, eidth, height
          drawRect = new Rectangle(0, 0, 0, 0);
       }
-      
+
+ //get width and height base off of the start and end
       int width = (int)Math.abs(posEnd.getX() - posStart.getX());
       int height = (int)Math.abs(posEnd.getY() - posStart.getY());
+//the smallest of the x and y is the top left
       int left = (int)Math.min(posStart.getX(), posEnd.getX());
       int top = (int)Math.min(posStart.getY(), posEnd.getY());
       
@@ -140,8 +240,22 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
       drawRect.setSize(width, height);
       
       // Let paintComponent handle this later
+      //otherwise nothign will showup
+      //everytime you want to change the image call this
       repaint();
    }
+
+
+   //Changing Circle
+   /*public void updateCircle(){
+
+      if (drawRound == null) {
+         drawRound = new Circle(0, 0, 0, 0);
+      }
+
+
+
+   }*/
    
    
    
--- ShapeDrawGUI.java
+++ ShapeDrawGUI.java
@@ -1,7 +1,11 @@
 
-
+//JFrame is in here
 import javax.swing.*;
 import java.awt.*;
+
+//import com.apple.eawt.ApplicationEvent;
+//import com.apple.eawt.ApplicationListener;
+
 
 
 
@@ -15,7 +19,8 @@ public class ShapeDrawGUI extends JFrame {
    private JButton rectangleButton;
    private JButton ovalButton;
    private JButton octagonButton;
-   
+
+
    
    public ShapeDrawGUI() {
       
@@ -23,12 +28,19 @@ public class ShapeDrawGUI extends JFrame {
       setLayout(new GridBagLayout());
       GridBagConstraints positionConst = new GridBagConstraints();
       positionConst.insets = new Insets(10, 10, 10, 10);
+
+
       
       // Set up the window
       setSize(1100,800);        
       setTitle("Draw Shapes");        
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
       
+      //making and adding buttons, check boxes an dsuch
+      //DON"T CHANGE THIS
       // Add the canvas
       positionConst.gridx = 0;
       positionConst.gridy = 0;
@@ -36,7 +48,10 @@ public class ShapeDrawGUI extends JFrame {
       positionConst.gridwidth = GridBagConstraints.REMAINDER;
       positionConst.weightx = 1;
       positionConst.weighty = 1;
-      
+
+
+
+      //the canvas is doing anything that is drawn
       canvas = new Canvas();        
       add(canvas, positionConst);
       
@@ -46,14 +61,15 @@ public class ShapeDrawGUI extends JFrame {
       positionConst.weighty = 0;
       
       // Add buttons and checkboxes below the canvas here
-     
+      // need command pattern
+
       // Create "Triangle" Button
       triangleButton = new JButton("Triangle");
+      //triangleButton.addActionListener(this);
       
-      
-      positionConst.gridx = 0;
-      positionConst.gridy = 1;
-      positionConst.insets = new Insets(10, 10, 10, 10);
+      positionConst.gridx = 1;
+      positionConst.gridy = 2;
+      positionConst.insets = new Insets(5, 5, 5, 5);
       add(triangleButton, positionConst);
       
       
@@ -61,9 +77,9 @@ public class ShapeDrawGUI extends JFrame {
       circleButton = new JButton("Circle");
      
       
-      positionConst.gridx = 0;
+      positionConst.gridx = 2;
       positionConst.gridy = 2;
-      positionConst.insets = new Insets(10, 10, 10, 10);
+      positionConst.insets = new Insets(5, 5, 5, 5);
       add(circleButton, positionConst);
       
       
@@ -71,9 +87,9 @@ public class ShapeDrawGUI extends JFrame {
       squareButton = new JButton("Square");
       
       
-      positionConst.gridx = 0;
+      positionConst.gridx = 3;
       positionConst.gridy = 2;
-      positionConst.insets = new Insets(10, 10, 10, 10);
+      positionConst.insets = new Insets(5, 5, 5, 5);
       add(squareButton, positionConst);
       
       
@@ -81,9 +97,9 @@ public class ShapeDrawGUI extends JFrame {
       rectangleButton = new JButton("Rectangle");
       
       
-      positionConst.gridx = 0;
-      positionConst.gridy = 2;
-      positionConst.insets = new Insets(10, 10, 10, 10);
+      positionConst.gridx = 1;
+      positionConst.gridy = 3;
+      positionConst.insets = new Insets(5, 5, 5, 5);
       add(rectangleButton, positionConst);
       
       
@@ -91,9 +107,9 @@ public class ShapeDrawGUI extends JFrame {
       ovalButton = new JButton("Oval");
       
       
-      positionConst.gridx = 0;
-      positionConst.gridy = 2;
-      positionConst.insets = new Insets(10, 10, 10, 10);
+      positionConst.gridx = 2;
+      positionConst.gridy = 3;
+      positionConst.insets = new Insets(5, 5, 5, 5);
       add(ovalButton, positionConst);
       
       
@@ -101,12 +117,32 @@ public class ShapeDrawGUI extends JFrame {
       octagonButton = new JButton("Octagon");
       
       
-      positionConst.gridx = 0;
-      positionConst.gridy = 2;
-      positionConst.insets = new Insets(10, 10, 10, 10);
+      positionConst.gridx =3;
+      positionConst.gridy = 3;
+      positionConst.insets = new Insets(5, 5, 5, 5);
       add(octagonButton, positionConst);
-   }
    
+   
+    //create filled checkbox
+    JCheckBox checkbox = new JCheckBox("filled", false);
+ 
+    positionConst.gridx =4;
+    positionConst.gridy = 2;
+    positionConst.insets = new Insets(5, 5, 5, 5);
+    add(checkbox, positionConst);
+      
+   }
+
+
+
+
+
+
+
+
+
+
+   //this is what main will look like
    public static void main(String[] args) {
        
       // Main frame
