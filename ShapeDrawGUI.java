73058bc110d71871cc355f4c9e21e42351da81c9
Harry Pinkerton <50304543+hp73@users.noreply.github.com>
2019-11-07 10:57:10 -0500

    Update ShapeDrawGUI.java

--- ShapeDrawGUI.java
+++ ShapeDrawGUI.java
@@ -1,7 +1,11 @@
 
-
+//JFrame is in here
 import javax.swing.*;
 import java.awt.*;
+
+//import com.apple.eawt.ApplicationEvent;
+//import com.apple.eawt.ApplicationListener;
+
 
 
 
@@ -15,6 +19,7 @@ public class ShapeDrawGUI extends JFrame {
    private JButton rectangleButton;
    private JButton ovalButton;
    private JButton octagonButton;
+<<<<<<< HEAD
    private JCheckBox filledButton;
    private JButton redButton;
    private JButton blueButton;
@@ -23,6 +28,10 @@ public class ShapeDrawGUI extends JFrame {
    private JButton yellowButton;
    private JButton purpleButton;
    
+=======
+
+
+>>>>>>> ff41c3f8c7d30da8b95144edf8fb4cf1b004f6f7
    
    public ShapeDrawGUI() {
       
@@ -30,12 +39,19 @@ public class ShapeDrawGUI extends JFrame {
       setLayout(new GridBagLayout());
       GridBagConstraints positionConst = new GridBagConstraints();
       positionConst.insets = new Insets(10, 10, 10, 10);
+
+
       
       // Set up the window
       setSize(1100,800);        
       setTitle("Draw Shapes");        
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
       
+      //making and adding buttons, check boxes an dsuch
+      //DON"T CHANGE THIS
       // Add the canvas
       positionConst.gridx = 0;
       positionConst.gridy = 0;
@@ -43,7 +59,10 @@ public class ShapeDrawGUI extends JFrame {
       positionConst.gridwidth = GridBagConstraints.REMAINDER;
       positionConst.weightx = 1;
       positionConst.weighty = 1;
-      
+
+
+
+      //the canvas is doing anything that is drawn
       canvas = new Canvas();        
       add(canvas, positionConst);
       
@@ -53,6 +72,7 @@ public class ShapeDrawGUI extends JFrame {
       positionConst.weighty = 0;
       
       // Add buttons and checkboxes below the canvas here
+<<<<<<< HEAD
      
       
       // Create "Triangle" Button
@@ -62,42 +82,80 @@ public class ShapeDrawGUI extends JFrame {
       positionConst.insets = new Insets(10, 10, 10, 10);
       positionConst.gridx = 0;
       positionConst.gridy = 1;
+=======
+      // need command pattern
+
+      // Create "Triangle" Button
+      triangleButton = new JButton("Triangle");
+      //triangleButton.addActionListener(this);
+      
+      positionConst.gridx = 1;
+      positionConst.gridy = 2;
+      positionConst.insets = new Insets(5, 5, 5, 5);
+>>>>>>> ff41c3f8c7d30da8b95144edf8fb4cf1b004f6f7
       add(triangleButton, positionConst);
       
       
       //Create "Circle" Button
       circleButton = new JButton("Circle");
       
+<<<<<<< HEAD
       positionConst.insets = new Insets(10, 10, 10, 10);
       positionConst.gridx = 1;
       positionConst.gridy = 1;
+=======
+      positionConst.gridx = 2;
+      positionConst.gridy = 2;
+      positionConst.insets = new Insets(5, 5, 5, 5);
+>>>>>>> ff41c3f8c7d30da8b95144edf8fb4cf1b004f6f7
       add(circleButton, positionConst);
       
       
       // Create "Square" Button
       squareButton = new JButton("Square");
       
+<<<<<<< HEAD
       positionConst.insets = new Insets(10, 10, 10, 10);
       positionConst.gridx = 2;
       positionConst.gridy = 1;
+=======
+      
+      positionConst.gridx = 3;
+      positionConst.gridy = 2;
+      positionConst.insets = new Insets(5, 5, 5, 5);
+>>>>>>> ff41c3f8c7d30da8b95144edf8fb4cf1b004f6f7
       add(squareButton, positionConst);
       
       
       // Create "Rectangle" Button
       rectangleButton = new JButton("Rectangle");
       
+<<<<<<< HEAD
       positionConst.insets = new Insets(10, 10, 10, 10);
       positionConst.gridx = 3;
       positionConst.gridy = 1;
+=======
+      
+      positionConst.gridx = 1;
+      positionConst.gridy = 3;
+      positionConst.insets = new Insets(5, 5, 5, 5);
+>>>>>>> ff41c3f8c7d30da8b95144edf8fb4cf1b004f6f7
       add(rectangleButton, positionConst);
       
       
       //Create "Oval" Button
       ovalButton = new JButton("Oval");
       
+<<<<<<< HEAD
       positionConst.insets = new Insets(10, 10, 10, 10);
       positionConst.gridx = 4;
       positionConst.gridy = 1;
+=======
+      
+      positionConst.gridx = 2;
+      positionConst.gridy = 3;
+      positionConst.insets = new Insets(5, 5, 5, 5);
+>>>>>>> ff41c3f8c7d30da8b95144edf8fb4cf1b004f6f7
       add(ovalButton, positionConst);
       
       
@@ -118,6 +176,7 @@ public class ShapeDrawGUI extends JFrame {
       add(filledButton, positionConst);
       
       
+<<<<<<< HEAD
       //Create "Red" Button
       redButton = new JButton("Red");
       
@@ -173,7 +232,34 @@ public class ShapeDrawGUI extends JFrame {
       
       
    }
+=======
+      positionConst.gridx =3;
+      positionConst.gridy = 3;
+      positionConst.insets = new Insets(5, 5, 5, 5);
+      add(octagonButton, positionConst);
+>>>>>>> ff41c3f8c7d30da8b95144edf8fb4cf1b004f6f7
    
+   
+    //create filled checkbox
+    JCheckBox checkbox = new JCheckBox("filled", false);
+ 
+    positionConst.gridx =4;
+    positionConst.gridy = 2;
+    positionConst.insets = new Insets(5, 5, 5, 5);
+    add(checkbox, positionConst);
+      
+   }
+
+
+
+
+
+
+
+
+
+
+   //this is what main will look like
    public static void main(String[] args) {
        
       // Main frame
