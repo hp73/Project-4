2e5278eec501d568b4537e588fcdec8f7ef99b4a
Laurie Jones <hannahljones98@gmail.com>
2019-11-08 15:54:14 -0500

    changing the name so iterator works
    just had to change the name so itll be recognized as a class

--- shapes/geometricAbstract.java
+++ shapes/geometricAbstract.java
@@ -4,7 +4,7 @@
  * @version 1.0 
 */
 
-public class geometricAbstract {
+public class GeometricAbstract {
  // Default variables
     public static final String DEFAULT_COLOR = "Black";
     public static final boolean DEFAULT_FILLED = true;
