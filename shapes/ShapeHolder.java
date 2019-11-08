a595320b81912f2f743ebeb9d440dd99ac68d27b
Laurie Jones <hannahljones98@gmail.com>
2019-11-08 10:15:29 -0500

    fixing itorator and itarable
    this i s just a basic move,haven't fixed it since meeting with matthews

--- shapes/ShapeHolder.java
+++ shapes/ShapeHolder.java
@@ -20,12 +20,29 @@ import java.util.*;
 import java.io.*; 
 //import shapes.*;
 
+//the iterable
+//integer is whatever you want to go over
+//iterable,geometricabstract>
 
-public class ShapeHolder {   
-    // The singleton instance variable
+
+//put the for loop in paintComponent
+   //theat for loop is what puts in on fancas
+   //draw and paint is the same thign
+   // seperate the view fromt eh data model
+   //repaint just ipdats the view
+   //clicks are modifying the data model
+   //iterator is implicitely called for you
+   //rhis is wher you can put the semicolin and things
+
+
+
+public class ShapeHolder implements Iterable<GeometricAbstract> {   
+    GeometricAbstract[] shapes = new GeometricAbstract[50];
     private static ShapeHolder instance;
-    private int index;
 
+    public GeometricIterator iterator() {
+        return new PointIterator(this);
+     }
     
     public static ShapeHolder getInstance() {
        if (instance == null) {
@@ -34,30 +51,55 @@ public class ShapeHolder {
        
        return instance;
     }
+}
  
-    private ShapeHolder() {
-        GeometricAbstract[] shapes = new GeometricAbstract[50];
-        Iterator iterator = ShapeHolder.iterator();
+//the iterator
+class GeometricIterator implements Iterable<GeometricAbstract> {
+    private int index = 0;
+    
+    /*public ShapeContainer() {
+       shapes = new shapes[] {new GeometricShape(),
+                               new Square(1, "Blue", 3, 4),
+                               new Square()};
+    }*/
+    
+    public GeometricIterator iterator() {
         index = 0;
-
+       return new GeometricIterator(this);
     }
-
+    
+ }
+ 
+ class GeometricIterator implements Iterator<Square> {
+    SquareContainer data;
+    int current;
+    
+    public SquareIterator(SquareContainer data) {
+       this.data = data;
+       current = 0;
+    }
+    
+    /*public Square next() {
+       return data.squares[current++];
+       
+    }*/
     private void next(){
         //this tells you  what is next in the Geometric shape array
         index ++; 
         System.out.println(shapes[index]);
 
     }
-
+    
+    /*public boolean hasNext() {
+       return current < data.squares.length;
+    }*/
     private void hasNext(){
         //checks where or not a there is room next in GeometricShapes has a next
         //returns string
-        //needs to check the next value but address thsi later
-
         String ok;
-        //shapes[index];
+        int length = shapes.length;
 
-        if(index +  1 <= shapes.length() ){
+        if(index +  1 <= length ){
             ok = "the next value is: " + index + 1;
         }
         else{
@@ -69,4 +111,6 @@ public class ShapeHolder {
 
     }
  }
+
+ 
  
